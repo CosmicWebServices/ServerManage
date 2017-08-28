5ea566103793036153dd75aee25a91aa8c561d85
herohamp <hampmoe@me.com>
2017-08-27 20:28:21 -0400

    Redesign

--- www/dist/js/main.js
+++ www/dist/js/main.js
@@ -1,5 +1,6 @@
 var sockets = [];
 var specs = [];
+var currentServer = 0;
 
 for (i in config.servers){
   sockets[i] = io.connect(config.servers[i][1]);
@@ -11,9 +12,8 @@ for (i in config.servers){
   sockets[i].on('cpuUsage', function(info){
     specs[info.i].cpu = info.cpu;
   })
-
-  $(".navbar-nav.mr-auto").append('<li class="nav-item"><a class="nav-link" href="#">'+ config.servers[i][0] +'</a></li>');
 }
+
 
 
 function tick15s(){
@@ -26,3 +26,8 @@ function tick15s(){
 
 tick15s();
 setInterval(tick15s, 15000)
+function drawNav(){
+  for (i in config.servers){
+    $(".treeview-menu").append('<li class="active"><a href="#"><i class="fa fa-circle-o"></i>'+config.servers[i][0]+'</a></li>');
+  }
+}
